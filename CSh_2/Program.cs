f3de00a93c7d6f0635ce57b2125e79185a46e713
Denis Korkishko <Game.den20@yandex.ru>
2020-05-04 05:01:06 +0300

    Добавьте файлы проекта.

---
+++ CSh_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSh_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write("\n");
+                for (int j = 1; j <= i; j++)
+                    Console.Write(j + "  ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie_2_4/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Zadanie_2_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nums = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nums
+            // 
+            this.nums.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nums.Location = new System.Drawing.Point(0, 0);
+            this.nums.Multiline = true;
+            this.nums.Name = "nums";
+            this.nums.Size = new System.Drawing.Size(491, 376);
+            this.nums.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 378);
+            this.Controls.Add(this.nums);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nums;
+    }
+}
+
